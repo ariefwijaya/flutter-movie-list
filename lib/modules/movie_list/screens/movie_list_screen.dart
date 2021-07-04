e4212ac6ebc9747d834f967858ac5edae4d86883
Arief Wijaya <ariefwiijaya@gmail.com>
2021-07-05 06:56:04 +0700

    added base page list and detail

---
+++ lib/modules/movie_list/screens/movie_list_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MovieListScreen extends StatefulWidget {
+  MovieListScreen({Key? key}) : super(key: key);
+
+  @override
+  _MovieListScreenState createState() => _MovieListScreenState();
+}
+
+class _MovieListScreenState extends State<MovieListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
