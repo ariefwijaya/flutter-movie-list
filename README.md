0084225bdf4a4bc6c7ae427f49c8ee52c9557803
Arief Wijaya <ariefwiijaya@gmail.com>
2021-07-05 06:02:31 +0700

    init commit and define packages

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_movie_list
+
+The POC to display a list of films from third parties.
---
+++ android/app/src/main/kotlin/com/etramatech/flutter_movie_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.etramatech.flutter_movie_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
